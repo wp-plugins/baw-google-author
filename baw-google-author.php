b0b8fce2bbe321e6109e81f0a67c23d7112256ca
juliobox <juliobox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-22 09:44:46 +0000

    1.1
    git-svn-id: https://plugins.svn.wordpress.org/baw-google-author/trunk@575672 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ baw-google-author.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Plugin Name: BAW Google Author
+Plugin URI: http://www.boiteaweb.fr/2929
+Description: Add a meta tag AUTHOR for SEO
+Version: 1.1
+Author: Juliobox
+Author URI: http://www.BoiteaWeb.fr
+License: GPLv2
+*/
+
+function bawga_l10n_init()
+{
+  load_plugin_textdomain( 'baw_ga', '', dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+}
+add_action( 'init', 'bawga_l10n_init' );
+
+function bawga_register_settings()
+{
+	register_setting( 'bawga-settings-group', 'bawga_authorname', 'esc_attr' );
+	register_setting( 'bawga-settings-group', 'bawga_authorstyle', 'esc_attr' );
+}
+add_action( 'admin_init', 'bawga_register_settings' );
+
+function bawga_create_menu()
+{
+	add_options_page( 'Google Author', 'Google Author', 'manage_options', 'baw_ga_config', 'bawga_page' );
+}
+add_action( 'admin_menu', 'bawga_create_menu' );
+
+function bawga_page()
+{
+	screen_icon( 'options-general' );
+?>
+<h2>Google Author</h2>
+<?php
+  settings_errors();
+?>
+<form method="post" action="options.php" id="bawga_form">
+
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><h3><?php _e( 'META author style', 'baw_ga' ); ?></h3></th>
+        <td>
+        <p><label><input type="radio" name="bawga_authorstyle" value="name" <?php checked( get_option( 'bawga_authorstyle' ), 'name' ); ?> /> <input type="text" name="bawga_authorname" value="<?php echo esc_attr( get_option( 'bawga_authorname' ) ); ?>" /></label><br />
+        <em><?php _e( 'Always use this name. Will be used too on every non post/page (index, 404, search, ...)', 'baw_ga' ); ?></em></p>
+        <p><label><input type="radio" name="bawga_authorstyle" value="auto" <?php checked( get_option( 'bawga_authorstyle' ), 'auto' ); ?> /> Auto</label><br />
+        <em><?php _e( 'Use the real author from post/page.', 'baw_ga' ); ?></em></p>
+        </td>
+        </tr>
+    </table>
+
+    <?php 
+	submit_button();
+	settings_fields( 'bawga-settings-group' );
+	include( dirname( __FILE__ ) . '/about.php' )
+	?>
+</form>
+<?php
+}
+
+function bawga_post_author( $atts, $content = '' )
+{
+	global $post;
+	extract(shortcode_atts(array(
+		"class" => '',
+		"link" => get_the_author_meta( 'user_url', $post->user_id ),
+		"text" => get_the_author_meta( 'user_nicename', $post->user_id )
+	), $atts));
+	$link = $link != '' ? $link : home_url( '/author/' . get_the_author_meta( 'user_nicename', $post->user_id ) );
+	$content = $content != '' ? $content : $text;
+	$content = '<a class="' . $class . '" rel="author" href="' . $link . '">' . $content . '</a>';
+	return $content;
+}
+add_shortcode( 'bawga_author', 'bawga_post_author' );
+
+function bawga_post_author2( $content )
+{
+	global $post;
+	$content = $content . get_the_author_link();
+	return $content;
+}
+add_action( 'the_content', 'bawga_post_author2' );
+
+function bawga_meta_author()
+{
+	global $post;
+	$user_info = get_userdata($post->post_author);
+	$author = $user_info->user_nicename;
+	$author = $author != '' && get_option( 'bawga_authorstyle' ) == 'auto' && ( is_single() || is_page() )? $author : esc_attr( get_option( 'bawga_authorname' ) );
+	echo '<meta rel="author" content="' . $author . '" />' . "\n";
+}
+add_action( 'wp_head', 'bawga_meta_author' );
+
+function bawga_settings_action_links( $links )
+{
+	$settings_link = '<a href="' . admin_url( 'admin.php?page=baw_ga_config' ) . '">' . __( 'Settings' ) . '</a>';
+	array_unshift( $links, $settings_link );
+	return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'bawga_settings_action_links' );
+
+function bawga_default_values()
+{
+	global $current_user;
+	$user_login = $current_user->user_nicename;
+	add_option( 'bawga_authorname', $user_login );
+	add_option( 'bawga_authorstyle', 'auto' );
+}
+register_activation_hook( __FILE__, 'bawga_default_values' );
+
+function bawga_uninstaller()
+{
+	delete_option( 'bawga_authorname' );
+	delete_option( 'bawga_authorstyle' );
+}
+register_uninstall_hook( __FILE__, 'bawga_uninstaller' );
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== BAW Google Author ===
+Contributors: juliobox
+Tags: google, author, meta, tag, seo
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: trunk
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RB7646G6NVPWU
+License: GPLv2
+
+Add a META tag "Author" for SEO reasons.
+
+== Description ==
+Add a META tag Author for SEO reasons. Author name can be automatic on each post/page or set to a choosen name.
+
+== Installation ==
+
+1. Extract the plugin folder from the downloaded ZIP file.
+2. Upload BAW Google Author folder to your /wp-content/plugins/ directory.
+3. Activate the plugin from the "Plugins" page in your Dashboard.
+4. Go to settings !
+
+== Frequently Asked Questions ==
+None
+
+== Screenshots ==
+None
+
+== Upgrade Notice ==
+Nothing
+
+== Changelog ==
+
+= 1.1 =
+* 22 jul 2012
+* value => content
+* remove BoiteAWeb parent menu
+* Code optimisation
+
+= 1.0 =
+* 09 jun 2011
+* First Release
